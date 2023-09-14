3b6d66b62c15b2e3551696769fffa6f2af4608b7
cedricd <cedricdecruw@gmail.com>
2023-09-14 17:04:20 +0200

    added N3 to parse triples that couldn't be parsed by rdflib

--- src/graph.js
+++ src/graph.js
@@ -10,9 +10,17 @@ export default class Graph {
     //make constructor
     constructor(data,entity,type) {
         //handle import of data
-        console.log(data);
-        this.graph_data = this.importData(data,entity,type);
+        //console.log(data);
+        this.pre_filtered_triples = this.importData(data,entity,type);
     }
+
+    callbackFunc = function(err, kb) {
+        if (err) {
+            console.log("Error while parsing: " + err);
+        } else {
+            console.log("Parsing succeeded. Knowledge base is: " + kb);
+        }
+    };
 
     //import data
     importData(data,entity,type) {
@@ -20,11 +28,26 @@ export default class Graph {
         console.log(data);
         //create a new graph object
         const graph = $rdf.graph();
-        //parse the data into the graph
-        $rdf.parse(data, graph, entity, type);
-        //return the graph
-        console.log(graph);
-        return graph;
-
+        try {
+            //parse the data into the graph
+            //if the data typer was application/json-ld use the callback function
+            if (type == 'application/json-ld') {
+                $rdf.parse(data, graph, entity, type, this.callbackFunc());
+            }
+            else{
+                $rdf.parse(data, graph, entity, type);
+            }
+            //return the graph
+            console.log(graph);
+            return {"rdflib":graph};
+        } catch (error) {
+            console.log(error);
+            //use N3.js to parse the data
+            const parser = new N3.Parser();
+            const quads = parser.parse(data);
+            console.log(quads);
+            //return the graph
+            return {"N3":quads};
+        }
     }
 }
