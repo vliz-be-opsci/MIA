5197d18aeb35c3908c41b93f3d827096df16bbe2
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-02-09 11:06:11 +0100

    Update deref_config.json with new prefixes and assertion paths

--- src/DerefInfoCollector.js
+++ src/DerefInfoCollector.js
@@ -1,99 +1,238 @@
-import { addDataToStore, createEmptyStore, getLinkedDataNQuads } from "./linked_data_store.js";
+import {
+  addDataToStore,
+  createEmptyStore,
+  getLinkedDataNQuads,
+} from "./linked_data_store.js";
 
 export default class DerefInfoCollector {
-    constructor(config) {
-        this.cashedInfo = {};
-        this.derefconfig = config;
-    }
+  constructor(config) {
+    this.cashedInfo = {};
+    this.derefconfig = config;
+  }
 
-    collectInfo(url) {
-        console.log('collecting info');
-        console.log(this.cashedInfo);
-        console.log(this.derefconfig);
-        console.log(url);
-        if (this.cashedInfo[url] !== undefined) {
-            console.log('info already collected');
-            return this.cashedInfo[url];
-        }
-        this.getInfoGraph(url);
-        return {
-            'url': url,
-            'info': 'info'
-        }
+  collectInfo(url) {
+    console.log("collecting info");
+    console.log(this.cashedInfo);
+    console.log(this.derefconfig);
+    console.log(url);
+    if (this.cashedInfo[url] !== undefined) {
+      console.log("info already collected");
+      return this.cashedInfo[url];
     }
+    this.getInfoGraph(url);
+    return {
+      url: url,
+      info: "info",
+    };
+  }
 
-    async getInfoGraph(url) {
-        let triplestore = createEmptyStore();
-        console.log('getting info graph');
-        getLinkedDataNQuads(url).then((triplestore) => {
-            console.log(triplestore);
-            //check via the derefconfig if all the required info is present 
-            //if not, fetch the missing info
-            console.log(this.getConfigInfoType(this.getTypes(url,triplestore)));
+  async getInfoGraph(url) {
+    let triplestore = createEmptyStore();
+    console.log("getting info graph");
+    getLinkedDataNQuads(url).then((triplestore) => {
+      console.log(triplestore);
+      let urls = [url];
+      if (url.startsWith("https")) {
+        urls.push(url.replace("https://", "http://"));
+      } else if (url.startsWith("http")) {
+        urls.push(url.replace("http://", "https://"));
+      }
+
+      for (const url of urls) {
+        this.getTypes(url, triplestore).then((types) => {
+          //check if length of types is not 0 else return;
+          if (types.length == 0) {
+            return;
+          }
+          console.log(types);
+          let type_config_url = this.getConfigInfoType(types);
+          console.log(type_config_url);
+          let prefixes = type_config_url.PREFIXES;
+          let assertion_paths = type_config_url.ASSERTION_PATHS;
+
+          let prefixed_assertion_paths = this.replacePrefixesAssertionPaths(
+            assertion_paths,
+            prefixes
+          );
+          console.log(prefixed_assertion_paths);
+          this.completeInfo(prefixed_assertion_paths, triplestore);
         });
-        return 
-    }
+      }
+    });
+    return;
+  }
 
-    
-    getTypes(url, triplestore) {
-        console.log('getting types');
-        //perform url trick for now that https and http are both checked, be sure to replace the beginning of the url only
-        try {
-            let urls = [url];
-            if (url.startsWith('https')) {
-                urls.push(url.replace('https://', 'http://'));
-            } else if (url.startsWith('http')) {
-                urls.push(url.replace('http://', 'https://'));
+  completeInfo(assertion_paths, triplestore) {
+    //check if all the paths are present in the triplestore
+    for (const assertion_path of assertion_paths) {
+      if (!this.assertPathsConfig(assertion_path, triplestore)) {
+        //fetch the missing info
+        console.log("fetching missing info");
+      }
+    }
+  }
+
+  replacePrefixesAssertionPaths(assertion_paths, prefixes) {
+    //replace the prefixes in the assertion paths
+    let new_assertion_paths = [];
+    for (const assertion_path of assertion_paths) {
+      // eg: replace "schema:" with "http://schema.org/" => schema:Person => <http://schema.org/Person>
+      // eg: "mr:geoObjects/mr:GeoObject" => "<http://mrgeo.com/geoObjects>/<http://mrgeo.com/geoObjects/GeoObject>
+      // prefixes [{prefix: "schema", uri: "http://schema.org/"}]
+      // perform regex expression to determine the parts that need to be replaced
+      // <http://www.w3.org/ns/dcat#centroid>/gsp:asWKT => ["<http://www.w3.org/ns/dcat#centroid>", "gsp:asWKT"]
+      // mr:hasGeometry/gsp:asWKT => ["mr:hasGeometry", "gsp:asWKT"]
+      let REGEXP = /<[^>]+>|[^/]+/g;
+      let parts_assertion_path = assertion_path.match(REGEXP);
+      console.log(parts_assertion_path);
+      let prefixed_parts_path = [];
+      for (const part of parts_assertion_path) {
+        let len_prefixed_parts_path = prefixed_parts_path.length;
+        let new_len = len_prefixed_parts_path;
+        for (const prefix of prefixes) {
+          if (len_prefixed_parts_path != new_len) {
+            break;
+          }
+          if (part.startsWith(prefix.prefix)) {
+            //replace prefix: with <prefix.uri> , don't forget to add the : at the end of the prefix
+            let new_part = part.replace(prefix.prefix + ":", "<" + prefix.uri);
+            new_part = new_part + ">"; // add the closing bracket
+            prefixed_parts_path.push(new_part);
+            new_len = len_prefixed_parts_path + 1;
+            //next in forloop
+            break;
+          }
+        }
+        //check if there was a part added, if not add the part as is
+        if (len_prefixed_parts_path == new_len) {
+          prefixed_parts_path.push(part);
+        }
+      }
+      new_assertion_paths.push(prefixed_parts_path.join("/"));
+    }
+    return new_assertion_paths;
+  }
+
+  assertPathsConfig(assertion_path, triplestore) {
+    //check if the path is present in the triplestore.
+    // return false if not
+    console.log("asserting paths config");
+    let query = `
+			SELECT ?s ?p ?o WHERE {
+				?s ${assertion_path} ?o .
+			}
+		`;
+    console.log(query);
+    const queryobject = $rdf.SPARQLToQuery(query, false, triplestore);
+    const results = triplestore.query(queryobject);
+    console.log(results);
+    if (results.length > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  async getTypes(url, triplestore) {
+    return new Promise((resolve, reject) => {
+      console.log("getting types with url: ", url);
+      try {
+        const types = [];
+        if (types.length == 0) {
+          const query = `
+						SELECT ?type WHERE {
+								<${url}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type .
+						}
+						.
+					`;
+          console.log(query);
+          const queryobject = $rdf.SPARQLToQuery(query, false, triplestore);
+          triplestore.query(queryobject, null, null, (results, error) => {
+            if (error) {
+              console.log("Error querying the triplestore:", error);
+              resolve([]);
+              return;
             }
-            const types = [];
-            // TODO: figure out why this is not working
-            // ERROR: Uncaught TypeError: t.pat is undefined
-            for (const url of urls) {
-                if (types.length == 0) {
-                    const query = `
+            if (!results) {
+              console.log("No results from the query");
+              resolve([]);
+              return;
+            }
+            console.log(results);
+            for (const result of results.bindings) {
+              types.push(result["?type"].value);
+            }
+            resolve(types);
+          });
+        }
+      } catch (error) {
+        console.log("error");
+        resolve([]);
+      }
+    });
+  }
+
+  getTypesOld(url, triplestore, callback) {
+    console.log("getting types");
+    //perform url trick for now that https and http are both checked, be sure to replace the beginning of the url only
+    try {
+      let urls = [url];
+      if (url.startsWith("https")) {
+        urls.push(url.replace("https://", "http://"));
+      } else if (url.startsWith("http")) {
+        urls.push(url.replace("http://", "https://"));
+      }
+      const types = [];
+      // TODO: figure out why this is not working
+      // ERROR: Uncaught TypeError: t.pat is undefined
+      for (const url of urls) {
+        if (types.length == 0) {
+          const query = `
                         SELECT ?type WHERE {
                             <${url}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type .
                         }
                     `;
-                    console.log(query);
-                    const results = triplestore.query(query);
-                    for (const result of results) {
-                        types.push(result.type.value);
-                    }
-                }
+          console.log(query);
+          const queryobject = $rdf.SPARQLToQuery(query, false, triplestore);
+          triplestore.query(queryobject, null, null, (results) => {
+            console.log(results);
+            for (const result of results.bindings) {
+              types.push(result["?type"].value);
             }
-            return types;
-        } catch (error) {
-            console.log('error');
-            let urls = [url];
-            if (url.startsWith('https')) {
-                urls.push(url.replace('https://', 'http://'));
-            } else if (url.startsWith('http')) {
-                urls.push(url.replace('http://', 'https://'));
-            }
-            const typeUri = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
-            const types = [];
-            for (const url of urls) {
-                const subject = $rdf.sym(url);
-                const predicate = $rdf.sym(typeUri);
-                const matches = triplestore.match(subject, predicate);
-                for (const match of matches) {
-                    types.push(match.object.value);
-                }
-            }
-            return types;
+            callback(types);
+          });
         }
-        
+      }
+      return types;
+    } catch (error) {
+      console.log("error");
+      let urls = [url];
+      if (url.startsWith("https")) {
+        urls.push(url.replace("https://", "http://"));
+      } else if (url.startsWith("http")) {
+        urls.push(url.replace("http://", "https://"));
+      }
+      const typeUri = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+      const types = [];
+      for (const url of urls) {
+        const subject = $rdf.sym(url);
+        const predicate = $rdf.sym(typeUri);
+        const matches = triplestore.match(subject, predicate);
+        for (const match of matches) {
+          types.push(match.object.value);
+        }
+      }
+      return types;
     }
+  }
 
-    getConfigInfoType(types){
-        for (const type of types) {
-            for( const config of this.derefconfig){
-                if(config.RDF_TYPE === type){
-                    return config;
-                }
-            }
+  getConfigInfoType(types) {
+    for (const type of types) {
+      for (const config of this.derefconfig) {
+        if (config.RDF_TYPE === type) {
+          return config;
         }
-        return null;
+      }
     }
+    return null;
+  }
 }
--- src/linked_data_store.js
+++ src/linked_data_store.js
@@ -45,123 +45,22 @@ export async function getLinkedDataNQuads(uri) {
     console.log(text);
 
     switch (data.format) {
-        case "application/ld+json":
-            $rdf.parse(text, store, uri, "application/ld+json");
-            break;
-        case "text/turtle":
-            $rdf.parse(text, store, uri, "text/turtle");
-            break;
-        case "text/html":
-            //search for fair signposting links in the head
-            parser = new DOMParser();
-            const doc = parser.parseFromString(text, "text/html");
-            //get all head links with rel describedby
-            const links = doc.querySelectorAll('head link[rel="describedby"]');
-            if (links.length > 0) {
-                // foreach link get the href and check if it has http in it not prepend the uri
-                // the format of the should be in the attribute type of the link , if not then check the extension of the href
-                // if the format is not supported then throw an error
-                for (const link of links) {
-                    let href = link.getAttribute("href");
-                    const type = link.getAttribute("type");
-                    //if href doesn't include http in the beginning then prepend the uri
-                    if (!href.includes("http")) {
-                        href = uri + href;
-                    }
-                    //fetch the href with the correct accept header
-                    const response = await fetch(href, {
-                        headers: {
-                            Accept: type,
-                        },
-                    });
-
-                    //get the text of the response
-                    text = await response.text();
-                    if (type === "application/ld+json") {
-                        $rdf.parse(text, store, href, "application/ld+json");
-                    } else {
-                        $rdf.parse(text, store, href, "text/turtle");
-                    }
-                }
-            } else {
-                throw new Error("No fair signposting links found");
-            }
-            break;
-        default:
-            try {
-                $rdf.parse(text, store, uri, data.format);
-            } catch (error) {
-                console.log(error);
-                throw new Error("Error parsing data");
-            }
-    }
-
-    // add data to store
-    // await addDataToStore(store, data);
-    // return the store
-    console.log(store);
-    return store;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export async function getLinkedDataNQuadsOld(uri) {
-  //flow of the function
-  // 1. check if the uri has a return format for ttl or json-ld
-  // 2. if it has a return format then fetch the uri with the correct accept header
-  // 3. if it doesn't ttl or jsonld then fetch the html page of the uri and search for fair signposting links in the head
-  // 4. if there are fair signposting links then fetch the uri with the correct accept header given in the fair signposting link
-
-  //check if the uri has a return format for ttl or json-ld
-  //order of preference for return formats
-  const return_formats = [
-    "text/turtle",
-    "application/ld+json",
-    "application/rdf+xml",
-    "application/n-triples",
-    "application/n-quads",
-    "text/n3",
-    "text/rdf+n3",
-    "text/html",
-  ];
-
-  try {
-    const data = await getData(uri, return_formats);
-    let text = await data.response.text();
-    //console.log(text);
-    //based on the format create a parser object
-    let parser;
-    let parsed_response;
-    switch (data.format) {
       case "application/ld+json":
-        try {
-          const jsontext = JSON.parse(text);
-          // Convert JSON-LD to N-Quads
-          const nquads = await jsonld.toRDF(jsontext, {
-            format: "application/n-quads",
-          });
-          // Parse N-Quads
-          parser = new N3.Parser({ format: "N-Quads" });
-          parsed_response = parser.parse(nquads);
-          break;
-        } catch (error) {
-          console.log(error);
-          throw new Error("Error parsing json-ld");
-        }
-      //have a case where the format is text/html and then search for fair signposting links
+        $rdf.parse(text, store, uri, "application/ld+json");
+        break;
+      case "text/turtle":
+        $rdf.parse(text, store, uri, "text/turtle");
+        break;
       case "text/html":
         //search for fair signposting links in the head
         parser = new DOMParser();
         const doc = parser.parseFromString(text, "text/html");
         //get all head links with rel describedby
-        // TODO: check if there are more types of links that can point ot a fair signposting link (describedby)
         const links = doc.querySelectorAll('head link[rel="describedby"]');
         if (links.length > 0) {
           // foreach link get the href and check if it has http in it not prepend the uri
           // the format of the should be in the attribute type of the link , if not then check the extension of the href
           // if the format is not supported then throw an error
-          let all_parsed_responses = [];
           for (const link of links) {
             let href = link.getAttribute("href");
             const type = link.getAttribute("type");
@@ -169,9 +68,6 @@ export async function getLinkedDataNQuadsOld(uri) {
             if (!href.includes("http")) {
               href = uri + href;
             }
-            //console.log(href);
-            //console.log(type);
-
             //fetch the href with the correct accept header
             const response = await fetch(href, {
               headers: {
@@ -181,67 +77,32 @@ export async function getLinkedDataNQuadsOld(uri) {
 
             //get the text of the response
             text = await response.text();
-            //console.log(text);
-            //create a n3 parser object and parse the response check first if the format is json-ld
-            //if it is json-ld then convert it to nquads
-            //if it is not json-ld then parse it with the correct format
             if (type === "application/ld+json") {
-              const jsontext = JSON.parse(text);
-              // Convert JSON-LD to N-Quads
-              const nquads = await jsonld.toRDF(jsontext, {
-                format: "application/n-quads",
-              });
-              // Parse N-Quads
-              parser = new N3.Parser({ format: "N-Quads" });
-              parsed_response = parser.parse(nquads);
-              //add the parsed response to the all_parsed_responses array but don't push
-              all_parsed_responses =
-                all_parsed_responses.concat(parsed_response);
+              $rdf.parse(text, store, href, "application/ld+json");
             } else {
-              parser = new N3.Parser({
-                format: type,
-                baseIRI: href,
-              });
-              parsed_response = parser.parse(text);
-              all_parsed_responses =
-                all_parsed_responses.concat(parsed_response);
+              $rdf.parse(text, store, href, "text/turtle");
             }
           }
-          //set parsed response to the imploded array of all_parsed_responses
-          parsed_response = all_parsed_responses;
         } else {
           throw new Error("No fair signposting links found");
         }
         break;
       default:
-        //create a n3 parser object and parse the response
-        parser = new N3.Parser({
-          format: data.format,
-          baseIRI: uri,
-        });
-        parsed_response = parser.parse(text);
+        try {
+          $rdf.parse(text, store, uri, data.format);
+        } catch (error) {
+          console.log(error);
+          throw new Error("Error parsing data");
+        }
     }
-    console.log(parsed_response);
 
-    return parsed_response;
+    // add data to store
+    // await addDataToStore(store, data);
+    // return the store
+    console.log(store);
+    return store;
   } catch (error) {
     console.log(error);
-  }
-}
-
-async function fetchData(fetcher, uri, formats) {
-  for (const format of formats) {
-    try {
-      const response = await fetch(uri, { headers: { Accept: format } });
-      const contentType = response.headers.get("Content-Type");
-
-      if (response.ok && contentType.includes(format)) {
-        fetcher.load(uri, { headers: { Accept: format } });
-        return;
-      }
-    } catch (error) {
-      console.log(error);
-    }
   }
 }
 
