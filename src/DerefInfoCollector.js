522d770e77e81552c76c1a742a2efa6ffb423240
cedricd <cedricdecruw@gmail.com>
2024-02-09 16:43:32 +0100

    Remove unused getTypesOld method

--- src/DerefInfoCollector.js
+++ src/DerefInfoCollector.js
@@ -159,60 +159,6 @@ export default class DerefInfoCollector {
     );
   }
 
-  getTypesOld(url, triplestore, callback) {
-    console.log("getting types");
-    //perform url trick for now that https and http are both checked, be sure to replace the beginning of the url only
-    try {
-      let urls = [url];
-      if (url.startsWith("https")) {
-        urls.push(url.replace("https://", "http://"));
-      } else if (url.startsWith("http")) {
-        urls.push(url.replace("http://", "https://"));
-      }
-      const types = [];
-      // TODO: figure out why this is not working
-      // ERROR: Uncaught TypeError: t.pat is undefined
-      for (const url of urls) {
-        if (types.length == 0) {
-          const query = `
-                        SELECT ?type WHERE {
-                            <${url}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type .
-                        }
-                    `;
-          console.log(query);
-          const queryobject = $rdf.SPARQLToQuery(query, false, triplestore);
-          triplestore.query(queryobject, null, null, (results) => {
-            console.log(results);
-            for (const result of results.bindings) {
-              types.push(result["?type"].value);
-            }
-            callback(types);
-          });
-        }
-      }
-      return types;
-    } catch (error) {
-      console.log("error");
-      let urls = [url];
-      if (url.startsWith("https")) {
-        urls.push(url.replace("https://", "http://"));
-      } else if (url.startsWith("http")) {
-        urls.push(url.replace("http://", "https://"));
-      }
-      const typeUri = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
-      const types = [];
-      for (const url of urls) {
-        const subject = $rdf.sym(url);
-        const predicate = $rdf.sym(typeUri);
-        const matches = triplestore.match(subject, predicate);
-        for (const match of matches) {
-          types.push(match.object.value);
-        }
-      }
-      return types;
-    }
-  }
-
   getConfigInfoType(types) {
     for (const type of types) {
       for (const config of this.derefconfig) {
